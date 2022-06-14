e122266cd3a333ba6b0b57af96cdb10ceb205394
chrisvdwerf <chrisvanderwerf@gmail.com>
2022-06-14 15:36:54 +0000

    finalize async

--- async_experiments.sh
+++ async_experiments.sh
@@ -2,8 +2,8 @@
 printf "terminating all previously running experiments .."
 screen -XS buffer quit; screen -XS denom quit; screen -XS maxtime quit; screen -XS grad quit
 
-printf "removing results folder.."
-rm experiments/maze/results
+printf "\nremoving results folder..\n"
+rm -rf experiments/maze/results
 
 # --- buffer ---
 export PYTHONPATH="${PYTHONPATH}:/AIDM_B_milestone_DQN/"
@@ -21,6 +21,6 @@ screen -S maxtimesteps -d -m bash -c './experiments/maze/configurations/maxtimes
 export PYTHONPATH="${PYTHONPATH}:/AIDM_B_milestone_DQN/"
 screen -S gradientsteps -d -m bash -c './experiments/maze/configurations/gradientsteps/run_experiments.sh; exec bash'
 
-printf "\n--- Started screen shells with experiments, they will terminate if completed --- \n\n"
+printf "\n--- Started screen shells with experiments, they will not terminate automatically (because of ending with exec bash) --- \n\n"
 screen -list
-printf "\nto terminate sessions: screen -XS buffer quit; screen -XS denom quit; screen -XS maxtime quit; screen -XS grad quit\n"
+printf "\n--- to terminate all sessions, execute:     screen -XS buffer quit; screen -XS denom quit; screen -XS maxtime quit; screen -XS grad quit\n"
