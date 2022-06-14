fe69ac3c65aeeb9e66f5098aa6e1cad51e03ce36
Mpcvanderwerf <m.p.c.vanderwerf@student.tudelft.nl>
2022-06-14 17:26:08 +0200

    fix folder

--- experiments/maze/configurations/gradientsteps/run_experiments.sh
+++ experiments/maze/configurations/gradientsteps/run_experiments.sh
@@ -5,7 +5,7 @@ for rep in 1 2 3 4 5
 do
     for value in 16 64 128
     do
-      cp experiments/maze/configurations/gradient_steps/15x15.yaml config_grad.yaml
+      cp experiments/maze/configurations/gradientsteps/15x15.yaml config_grad.yaml
             head -n -2 config_grad.yaml > temp_grad.txt ; mv temp_grad.txt config_grad.yaml
       echo "seed: $RANDOM" >> config_grad.yaml
       echo "gradient_steps: $value" >> config_grad.yaml
