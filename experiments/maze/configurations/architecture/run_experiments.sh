0a3d6e1c0fb8b92d36d1a103e013e62529fef2da
Mpcvanderwerf <m.p.c.vanderwerf@student.tudelft.nl>
2022-06-15 15:42:30 +0200

    update experiment

--- experiments/maze/configurations/architecture/run_experiments.sh
+++ experiments/maze/configurations/architecture/run_experiments.sh
@@ -16,10 +16,11 @@ RANDOM=$$
 for rep in 1 2 3 4 5
 do
       cp experiments/maze/configurations/architecture/15x15.yaml config_arch.yaml
-            head -n -2 config_arch.yaml > temp_arch.txt ; mv temp_arch.txt config_arch.yaml
+            head -n -4 config_arch.yaml > temp_arch.txt ; mv temp_arch.txt config_arch.yaml
       echo -e "  net_arch: [$1, $1, $1]" >> config_arch.yaml
       echo "seed: $RANDOM" >> config_arch.yaml
       echo "exploration_method: '$2'" >> config_arch.yaml
+      echo "results_folder: '$1_$2'" >> config_arch.yaml
       python3 experiments/maze/main.py -c config_arch.yaml
 done
 rm config_arch.yaml
