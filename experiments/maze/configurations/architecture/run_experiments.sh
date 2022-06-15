d9d8a398800264630d51ad8aa302105ca3210391
Mpcvanderwerf <m.p.c.vanderwerf@student.tudelft.nl>
2022-06-15 15:38:35 +0200

    further parameterize experiment

--- experiments/maze/configurations/architecture/run_experiments.sh
+++ experiments/maze/configurations/architecture/run_experiments.sh
@@ -6,17 +6,20 @@ if [ -z "$1" ]
     exit 0
 fi
 
+if [ -z "$2" ]
+  then
+    echo "No exploration method provided"
+    exit 0
+fi
+
 RANDOM=$$
 for rep in 1 2 3 4 5
 do
-    for value in 'adaptive4' 'traditional_milestones'
-    do
       cp experiments/maze/configurations/architecture/15x15.yaml config_arch.yaml
             head -n -2 config_arch.yaml > temp_arch.txt ; mv temp_arch.txt config_arch.yaml
       echo -e "  net_arch: [$1, $1, $1]" >> config_arch.yaml
       echo "seed: $RANDOM" >> config_arch.yaml
-      echo "exploration_method: $value" >> config_arch.yaml
+      echo "exploration_method: '$2'" >> config_arch.yaml
       python3 experiments/maze/main.py -c config_arch.yaml
-    done
 done
 rm config_arch.yaml
